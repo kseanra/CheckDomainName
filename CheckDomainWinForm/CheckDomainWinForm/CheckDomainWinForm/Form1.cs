7fd6064a72b2d9edf80f6df9dc04c345e6404c59
Ken Liang <KenL@AttacheSoftware.com>
2016-11-05 10:29:34 +1300

    Initalize commit

---
+++ CheckDomainWinForm/CheckDomainWinForm/CheckDomainWinForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace CheckDomainWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string ComputerDomain = Domain.GetComputerDomain() == null ? "" : Domain.GetComputerDomain().ToString().ToUpper();
+                MessageBox.Show(ComputerDomain);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+    }
+}
