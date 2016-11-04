089834da2cc8e6c0996b01ea5236022c58098c71
Ken Liang <KenL@AttacheSoftware.com>
2016-11-05 10:30:19 +1300

    Initalize Commit

--- CheckDomainWinForm/CheckDomainWinForm/CheckDomainWinForm/Form1.cs
+++ CheckDomainWinForm/CheckDomainWinForm/CheckDomainWinForm/Form1.cs
@@ -21,6 +21,7 @@ namespace CheckDomainWinForm
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             try
             {
                 string ComputerDomain = Domain.GetComputerDomain() == null ? "" : Domain.GetComputerDomain().ToString().ToUpper();
